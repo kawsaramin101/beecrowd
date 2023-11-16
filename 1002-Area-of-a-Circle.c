bb3ad1f6b3a84f359dc37a6e72abd06c31e24e76
kawsaramin101 <71335212+kawsaramin101@users.noreply.github.com>
2023-11-16 19:38:45 +0600

    Create 1002-Area-of-a-Circle.c

---
+++ 1002-Area-of-a-Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double A,n, R;
+    n = 3.14159;
+    scanf("%lf", &R);
+    
+    A = n*R*R;
+    
+    printf("A=%5.4lf", A);
+    
+    printf("\n");
+    return 0;
+}
